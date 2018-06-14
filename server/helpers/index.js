62c53e9e5468b7ebad79a9fa00e43758a35812b7
Eduard <edwardintoronto@gmail.com>
2018-06-14 12:26:54 -0500

    Combined register and login helpers into one file

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,3 +1,32 @@
+function error(err, res, mode){
+  if(mode == 'register'){
+    if(err.name === 'NoSaltValueStoredError'){
+      return res.json({
+        error: true,
+        message: err.message ? err.message : 'Sorry, an error occurred'
+      });
+    } else if(err.code === 11000){
+      // 11000 is duplicate username error. In Mongoose schema
+      // email is used as the usernameField hence the following
+      // error message
+      return res.json({
+        error: true,
+        message: 'This username already registered'
+      });
+    } else if (err.name === 'UserExistsError'){
+      // Because in Mongoose schema email is the usernamefield
+      // duplicate usernames means duplicate emails.
+      return res.json({
+        error: true,
+        message: 'This email already registered'
+      });      
+    } else {
+      respond(res, err.message);
+    }    
+  }
+}
+
+/*
 function errors(err, res, query, Account){
   console.log(`inside errors helpers`);
   console.log(err.name);
@@ -24,13 +53,16 @@ function respond(res, msg){
     message: msg ? msg : "Sorry, there was an error"
   });
 }
+*/
 
+/*
 function success(res){
   return res.json({
     error: false,
     message: "Registration successful. Please update your profile!"
   });
 }
+*/
 
 function passwordMismatch(res){
   return res.json({
@@ -40,7 +72,6 @@ function passwordMismatch(res){
 }
 
 module.exports = {
-  errors,
-  success,
+  error,
   passwordMismatch
 };
